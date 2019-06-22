e6b8d0a446532e48406a64b7d0589a82de100800
AMDLnghrns <aduncan001@gmail.com>
2019-06-22 17:45:02 -0500

    added in scripts and models

--- models/customer.js
+++ models/customer.js
@@ -1,14 +1,49 @@
-// Customer model
-
-// The Customer has a "customer" attribute of type DataTypes.String
-
 module.exports = function(sequelize, DataTypes) {
-  var Customer = sequelize.define("Customer", {
-    customer: {
-      type: DataTypes.STRING,
-      // If a customer is to be created, they must have a name
+  let Customer = sequelize.define("Customer", {
+    unique_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    firstname: {
+      type: DataTypes.STRING(50),
       allowNull: false
-    }
+    },
+    lastname: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    phonenumber: {
+      type: DataTypes.STRING(12),
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    address1: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    address2: DataTypes.STRING(100),
+    address3: DataTypes.STRING(100),
+    city: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    },
+    zip: DataTypes.STRING(10)
+  },{
+    freezeTableName: true,
+    tableName: 'Customer'
   });
   return Customer;
 };
