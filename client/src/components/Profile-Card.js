5b368dca2cdb17b8a3f8971693c52580570bca53
owaisj <jamil.owais@gmail.com>
2019-06-24 13:23:47 -0500

    Styles edited; functional component from class

--- client/src/components/Profile-Card.js
+++ client/src/components/Profile-Card.js
@@ -1,10 +1,8 @@
-import React, { Component } from "react";
+import React, { Fragment } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+import Paper from '@material-ui/core/Paper'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
 
 const useStyles = makeStyles({
   card: {
@@ -21,67 +19,29 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
+  paper: {
+    padding: '1em',
+    textAlign: 'center'
+  }
 });
 
-//NOTE: This is the old version of the component
-class ProfileTest extends Component {
-  constructor() {
-    super();
-    this.state = {
-      userInfo: {
-        fname: "",
-        lname: "",
-        email: "",
-        address1: "",
-        city: "",
-        state: ""
-      }
-    };
-    this.grabInformation = this.grabInformation.bind(this);
-  }
-  grabInformation (attr, val) {
-    fetch(`/api/customers/${attr}/${val}`, {
-      method: "GET"
-    })
-      .then(res => res.json())
-      .then(json => {
-        let u = json[0];
-        this.setState({
-          userInfo: {
-            fname: u.firstname,
-            lname: u.lastname,
-            address1: u.address1,
-            city: u.city,
-            state: u.state
-          }
-        });
-        console.log(this.state);
-      });
-  }
-  render() {
-    let profile = this.state.userInfo;
-    return (
-      <div>
-        <ul>
-          <li>
-            {profile.lname}, {profile.fname}
-          </li>
-          <li>
-            {profile.address1}
-          </li>
-          <li>
-            {profile.city}, {profile.state}
-          </li>
-        </ul>
-        <Button onClick={() => this.grabInformation("firstname", "Anthony")}>
-          Refresh Your Profile
-        </Button>
-      </div>
-    );
-  }
-}
-
-export default ProfileTest;
-
-//TODO: DisplayProfile - GET from db
-//From Login -> email, <value>
+export default function ProfileCard() {
+  const classes = useStyles();
+  return (
+    <Fragment>
+      <Paper className={classes.paper}>
+        <List>
+          <ListItem>
+            Name
+          </ListItem>
+          <ListItem>
+            Email
+          </ListItem>
+          <ListItem>
+            City, State Zip
+          </ListItem>
+        </List>
+      </Paper>
+    </Fragment>
+  )
+};
