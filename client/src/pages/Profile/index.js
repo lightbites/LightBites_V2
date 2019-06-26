397a5c1e42545fb828b58a0492bb5db4adcb7312
r8yt <hendersonkevin64@gmail.com>
2019-06-26 06:43:25 -0500

    starting changes

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -4,13 +4,13 @@ import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Header from "../../components/Header-Welcome";
 //import ProfileCard from "../../components/Profile-Card";
-import useFetch from '../Cart/Hooks'
+import useFetch from "../Cart/Hooks";
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    margin: '1em',
-    alignItems: 'center'
+    margin: "1em",
+    alignItems: "center"
   },
   container: {
     marginTop: "5%"
@@ -24,51 +24,51 @@ const useStyles = makeStyles(theme => ({
     textAlign: "left"
   },
   pageContainer: {
-    width: '500px',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'center'
+    width: "500px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center"
   }
 }));
 
 export default function Profile() {
-  const [data, loading] = useFetch('https://lightbites.herokuapp.com/api/customers/')
+  const [data, loading] = useFetch("https://lightbites.herokuapp.com/api/customers/");
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <Header />
       <div className={classes.pageContainer}>
-      <Grid className={classes.container} container spacing={3}>
-        <Grid item xs={6}>
-        <Paper className={classes.paper}>Name</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>Button</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>Address</Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>
+        <Grid className={classes.container} container spacing={3}>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>Name</Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>Button</Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>Address</Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
             Button
-          </Paper>
-        </Grid>
-        <Grid item xs={4}>
-          <Paper className={classes.paper}>
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper className={classes.paper}>
             City, State
-          </Paper>
-        </Grid>
-        <Grid item xs={2}>
-          <Paper className={classes.paper}>
+            </Paper>
+          </Grid>
+          <Grid item xs={2}>
+            <Paper className={classes.paper}>
             Zip
-          </Paper>
-        </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
             Button
-          </Paper>
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
       </div>
     </div>
   );
