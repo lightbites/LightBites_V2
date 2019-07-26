2f24589ecad59d0c066f1e2db3b999256badec2f
Justin Mozley <jtmozley@gmail.com>
2019-07-25 21:10:02 -0500

    Format Page
    Formatted code on Welcome/index.js using prettier

--- client/src/pages/Welcome/index.js
+++ client/src/pages/Welcome/index.js
@@ -6,15 +6,13 @@ import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 
-
-
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
     paddingLeft: "1%"
   },
   card: {
-    maxWidth: 345,
+    maxWidth: 345
   },
   container: {
     marginTop: "5%"
@@ -22,14 +20,14 @@ const useStyles = makeStyles(theme => ({
   paper: {
     padding: theme.spacing(2),
     textAlign: "center",
-    color: theme.palette.text.secondary,
+    color: theme.palette.text.secondary
   },
   button: {
     ...theme.typography.button,
-    margin: theme.spacing(1),
+    margin: theme.spacing(1)
   },
   input: {
-    display: "none",
+    display: "none"
   },
 
   text: {
@@ -59,21 +57,34 @@ export default function CenteredGrid() {
       <Container className={classes.container} maxWidth="lg">
         <Grid container spacing={3}>
           <br />
-          
+
           <Grid item xs={6}>
             <h1 className={classes.heading}>Light Bites</h1>
             <div className={classes.cta}>
-              <h4 className={classes.headline}>Our Mission is to provide our community with fresh, nutritious meal options for the busy lifestyle.</h4>
-              <Divider/>
-              <p>Whether you're on the go or looking to get into shape, Light Bites has got you covered. It's as simple as choosing your meals for the week, placing a delivery, and now you have food pre-made for you without the fuss of going into a grocery store.</p> 
-              <br /><Link to="/signup"><Button variant="contained" color="primary" className={classes.button}>
-            Lets Get Started
-              </Button></Link></div>
+              <h4 className={classes.headline}>
+                Our Mission is to provide our community with fresh, nutritious
+                meal options for the busy lifestyle.
+              </h4>
+              <Divider />
+              <p>
+                Whether you're on the go or looking to get into shape, Light
+                Bites has got you covered. It's as simple as choosing your meals
+                for the week, placing a delivery, and now you have food pre-made
+                for you without the fuss of going into a grocery store.
+              </p>
+              <br />
+              <Link to="/signup">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  className={classes.button}
+                >
+                  Lets Get Started
+                </Button>
+              </Link>
+            </div>
           </Grid>
-         
-          
         </Grid>
-  
       </Container>
     </div>
   );
